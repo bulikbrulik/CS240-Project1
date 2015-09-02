bf70fcf86b29c9335acc9111e16b75039e5233a5
bulikbrulik <bulik.brulik@gmail.com>
2015-09-02 16:00:26 -0600

    Add classes

---
+++ ImageEditor.java
@@ -0,0 +1,24 @@
+package project1;
+
+public class ImageEditor
+{
+	public static void main(String[] args)
+	{
+		if(args.length == 3)
+		{
+			switch(args[2])
+			{
+				case "invert":
+					break;
+				case "grayscale":
+					break;
+				case "emboss":
+					break;
+				case "speedblur":
+					break;
+				default:
+					break; 
+			}
+		}
+	}
+}
